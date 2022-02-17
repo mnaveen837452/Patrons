92b83c32fc30304b2d87e272b70f8a3b6f7373a2
mnaveen837452 <79991913+mnaveen837452@users.noreply.github.com>
2022-02-17 11:56:26 +0530

    Update PosApplication.java

--- POS/src/main/java/com/patrons/pos/PosApplication.java
+++ POS/src/main/java/com/patrons/pos/PosApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PosApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Wel come to pOS");
 		SpringApplication.run(PosApplication.class, args);
 	}
 
